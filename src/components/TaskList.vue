5fa778fd4d4479dc12ec7bd9d77984fbdbcb4e74
lealexio <alexleloup@yahoo.fr>
2021-03-25 15:51:04 +0100

    Update project

---
+++ src/components/TaskList.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="task-list"> 
+      <h1>Marina la plus belle</h1>
+      <div class="input-group mb-2">
+          <div class="input-group-prepend">
+              <div class="input-group-text">New task :</div>
+          </div>
+          <input v-model="todoInput" placeholder="task name" class="form-control border border-dark">
+          <div class="input-group-append">
+              <div class="btn-group ml-1" role="group" aria-label="Basic example">
+                  <button v-on:click="addTask(newtask)" class="btn btn-outline-secondary">Add</button>
+                  <button v-on:click="removeAllTasks()" class="btn btn-outline-secondary">Remove All</button>
+                  <button v-on:click="removeDoneTasks()" class="btn btn-outline-secondary">Remove done tasks</button>
+                  <select v-model="selectedFilter" v-on:change="updateListView()" class="form-select btn btn-outline-secondary" aria-label="Default select example">
+                      <option selected value="0">All</option>
+                      <option value="1">Done</option>
+                      <option value="2">To do</option>
+                  </select>
+              </div>
+          </div>
+      </div>
+
+      <ul class="list-group list-group-flush border">
+          <li v-for="(todo, index) in tasksListView" :key="index" class="list-group-item" :class="{'list-group-item-line-through list-group-item-danger': todo.checked}" >
+              <div class="input-group">
+                  <div class="input-group-prepend">
+                      <div class="input-group-text">
+                          <input type="checkbox" v-model="todo.checked" @click="todo.toggleChecked();updateTasksStats()">
+                      </div>
+                  </div>
+                  <input class="form-control w-50 float-center" v-bind:value="todo.todo" v-on:change="updateTaskName($event,todo)">
+                  <div class="input-group-append">
+                      <button type="button" class="btn btn-outline-secondary" aria-label="Close" @click.stop="deleteTask(todo)">x</button>
+                  </div>
+              </div>
+          </li>
+          <li v-if="nbtasks > 0" class="list-group-item list-group-item-secondary border border-dark">You have {{nbtasks}} tasks, {{nbTasksDone}} done and {{nbTasksToDo}} to do.</li>
+          <li v-else class="list-group-item list-group-item-secondary border border-dark">You have no tasks.</li>
+      </ul>
+  </div>
+</template>
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  class Todo {
+      todo: string;
+      checked: boolean;
+
+      constructor(todo: string) {
+          this.todo=todo;
+          this.checked=false;
+      }
+
+      toggleChecked(){
+          this.checked = !this.checked;
+      }
+  }
+
+  @Component({})
+  export default class TaskListComponent extends Vue {
+      todoInput: string | undefined;
+      tasksList: Todo[] = [];
+      tasksListView: Todo[] = [];
+      nbtasks = this.tasksList.length;
+      nbTasksDone = 0;
+      nbTasksToDo = 0;
+      selectedFilter = '0';
+
+      addTask() {
+          console.log("Add new task " + this.todoInput)
+          if (this.todoInput != undefined && this.todoInput != "") {
+              this.tasksList.push(new Todo(this.todoInput))
+              this.updateListView();
+          }
+      }
+
+      deleteTask(todo: Todo) {
+          console.log("Remove " + todo)
+          this.tasksList = this.tasksList.filter((todoItem) => {
+              return todoItem !== todo;
+          })
+          this.updateListView();
+      }
+
+      removeAllTasks() {
+          console.log("Remove all tasks")
+          this.tasksList = [];
+          this.updateListView();
+      }
+
+      updateTasksStats() {
+          console.log("Refresh task stats")
+          this.nbTasksDone = 0;
+          this.nbTasksToDo = 0;
+          for (const entry of this.tasksList) {
+              if (entry.checked) {
+                  this.nbTasksDone++;
+              } else {
+                  this.nbTasksToDo++;
+              }
+          }
+          this.nbtasks = this.tasksList.length;
+      }
+
+      updateListView() {
+          console.log("Update list view")
+          if(this.selectedFilter=='1'){
+              this.tasksListView = this.tasksList.filter(todo => todo.checked);
+              console.log(this.tasksListView.length);
+          }
+          else if(this.selectedFilter=='2'){
+              this.tasksListView = this.tasksList.filter(todo => !todo.checked);
+              console.log(this.tasksListView.length);
+          }
+          else{
+              this.tasksListView = this.tasksList;
+              console.log(this.tasksListView.length);
+          }
+          this.updateTasksStats();
+      }
+
+      removeDoneTasks(){
+          console.log("Remove done tasks")
+          this.tasksList = this.tasksList.filter(todo => !todo.checked);
+          this.updateListView();
+      }
+
+      updateTaskName(value: Event, todo: Todo){
+          todo.todo=value.target.value;
+          console.log(value.target.value);
+          this.updateListView();
+      }
+
+
+  }
+
+
+</script>
+
+<style>
+    .list-group-item-line-through{
+        text-decoration: line-through;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home container">
+    <!--
+    <p v-if="visible">Maintenant vous me voyez {{message}}</p>
+    <button v-on:click="foo()">ok</button>
+    <div v-background>Undefined color</div>
+    <div v-background="green">Defined color</div>
+    -->
+
+    <TaskListComponent/>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import { Component, Vue } from 'vue-property-decorator';
+  import {Background} from '@/directives/Background.directive'
+  import TaskListComponent from "@/components/TaskList.vue";
+  @Component({
+    components: {
+      TaskListComponent,
+    },
+    directives:{
+      Background
+    }
+  })
+  export default class Home extends Vue {
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+    message= 'test';
+    visible = true;
+    foo(){
+      this.visible = !this.visible;
+    }
+  }
 </script>
